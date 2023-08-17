14dcc60cb619e400b15f3a704068764664dff0cf
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-08-17 13:46:50 -0500

    гзвфеу

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { SearchBar } from './Searchbar/Searchbar';
 import { LoadMoreBtn } from './Button/Button';
+import { ImageGallery } from './ImageGallery/ImageGallery';
 
 export class App extends Component {
-  state = {};
+  state = {
+    query: '',
+    images: [],
+  };
+
+  // changeQuery = () => {};
+  // setImages = () => {};
 
   render() {
     return (
       <div>
         <SearchBar />
+        <ImageGallery />
         <LoadMoreBtn />
         <GlobalStyle />
       </div>
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
 import {
   StyledSearchInput,
   StyledSearchBtn,
   StyledSearchForm,
 } from './Searchbar.styled';
 
-export class SearchBar extends Component {
-  state = {};
-
-  render() {
-    return (
-      <div>
-        <header>
-          <StyledSearchForm>
-            <StyledSearchInput
-              type="text"
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images..."
-            />
-            <StyledSearchBtn type="submit">Search</StyledSearchBtn>
-          </StyledSearchForm>
-        </header>
-      </div>
-    );
-  }
-}
+export const SearchBar = () => {
+  return (
+    <div>
+      <header>
+        <StyledSearchForm
+          onSubmit={e => {
+            e.preventDefault();
+            console.log('Form submitted');
+          }}
+        >
+          <StyledSearchInput
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images..."
+          />
+          <StyledSearchBtn type="submit">Submit</StyledSearchBtn>
+        </StyledSearchForm>
+      </header>
+    </div>
+  );
+};
